f4d6d970d67b53cc02d93c0280cba80d3c5b5e75
zeepk <zeepkdb@gmail.com>
2021-01-23 17:34:35 -0800

    added cors handling

--- Startup.cs
+++ Startup.cs
@@ -36,11 +36,18 @@ namespace dotnet5_webapp
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "dotnet5_webapp", Version = "v1" });
             });
             services.AddSingleton<IUserService, UserService>();
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+   {
+       builder.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+   }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("MyPolicy");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
