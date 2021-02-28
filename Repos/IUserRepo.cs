1f857c9596edd5285e8e26e41ee3b07bc6408b93
Matt Hughes <zeepkdb@gmail.com>
2021-02-28 00:27:54 -0800

    * UserRepo.cs: * IUserRepo.cs: * UserSearchResponse.cs:
    * appsettings.json: added repo layer for db calls

---
+++ Repos/IUserRepo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using dotnet5_webapp.Models;
+
+namespace dotnet5_webapp.Repos
+{
+    public interface IUserRepo
+    {
+        Task<User> GetUserByUsername(string username);
+    }
+}
---
+++ Repos/UserRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet5_webapp.Data;
+using dotnet5_webapp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet5_webapp.Repos
+{
+    public class UserRepo : IUserRepo
+    {
+
+        private readonly DataContext Context;
+        public UserRepo(DataContext context) => Context = context;
+
+        public async Task<User> GetUserByUsername(string username)
+        {
+            var user = await Context.User.FirstOrDefaultAsync(u => u.Username == username);
+            return user;
+        }
+    
+    }
+}
